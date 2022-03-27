6c5d1afc14aa0003706d58c6d7b0e1fcb96412ef
Caleb <calebluke01@gmail.com>
2022-03-27 22:17:26 +0100

    Deploy gh-pages

---
+++ out/_next/static/jtsBeh3CTbsr3X--uNbUJ/_buildManifest.js
@@ -0,0 +1,1 @@
+self.__BUILD_MANIFEST={__rewrites:{beforeFiles:[],afterFiles:[],fallback:[]},"/":["static/chunks/881-9951c5b93e414f0c.js","static/chunks/pages/index-9b55e24611fe69a3.js","static/css/afa779690db171c4.css"],"/_error":["static/chunks/pages/_error-e526a695839b5a36.js"],sortedPages:["/","/_app","/_error"]},self.__BUILD_MANIFEST_CB&&self.__BUILD_MANIFEST_CB();
